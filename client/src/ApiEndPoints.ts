00d172d533d24b9da2068ac5d74924484f78d2a6
Uttam <ut25122001@gmail.com>
2024-06-09 16:28:00 +0530

    url updated

--- client/src/ApiEndPoints.ts
+++ client/src/ApiEndPoints.ts
@@ -1,3 +1,3 @@
 
-export const BaseUrl = "http://100.109.207.135:3000/api";
-export const SocketUrl = "http://100.109.207.135:3000"
+export const BaseUrl = "http://localhost:3000/api";
+export const SocketUrl = "http://localhost:3000"
